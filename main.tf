8e5ebe90f7ede1576cf8f2153b46277f920168ff
dibya <dibyasachi001@gmail.com>
2024-11-21 11:51:28 +0530

    Changed URLs in main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "VPC_module" {
 #  source   = "./module_vpc1st/VPC_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/VPC_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/VPC_module"
   vpc_cidr = var.vpc_cidr
   prefix   = var.prefix_vpc
 }
 
 module "Pub_Sub_module" {
   # source             = "./module_vpc1st/Pub_Sub_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/Pub_Sub_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/Pub_Sub_module"
   vpc_id             = module.VPC_module.vpc_id
   pub_sub_cidr       = var.pub_sub_cidr
   availability_zones = var.availability_zones
@@ -15,7 +15,7 @@ module "Pub_Sub_module" {
 
 module "Pvt_Sub_module" {
   # source             = "./module_vpc1st/Pvt_Sub_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/Pvt_Sub_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/Pvt_Sub_module"
   vpc_id             = module.VPC_module.vpc_id
   pvt_sub_cidr       = var.pvt_sub_cidr
   availability_zones = var.availability_zones
@@ -23,14 +23,14 @@ module "Pvt_Sub_module" {
 
 module "IGW_module" {
   # source     = "./module_vpc1st/IGW_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/IGW_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/IGW_module"
   vpc_id     = module.VPC_module.vpc_id
   prefix_vpc = var.prefix_vpc
 }
 
 module "Pub_rt_module" {
   # source       = "./module_vpc1st/Pub_rt_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/Pub_rt_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/Pub_rt_module"
   vpc_id       = module.VPC_module.vpc_id
   igw_id       = module.IGW_module.internet_gateway_id
   subnetids    = module.Pub_Sub_module.public_subnet_ids
@@ -39,7 +39,7 @@ module "Pub_rt_module" {
 
 module "Pvt_rt_module" {
   # source       = "./module_vpc1st/Pvt_rt_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/Pvt_rt_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/Pvt_rt_module"
   vpc_id       = module.VPC_module.vpc_id
   subnetids    = module.Pvt_Sub_module.private_subnet_ids
   prefix_pvtrt = var.prefix_pvtrt
@@ -47,7 +47,7 @@ module "Pvt_rt_module" {
 
 module "SG_module" {
   # source     = "./module_vpc1st/SG_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/SG_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/SG_module"
   vpc_id     = module.VPC_module.vpc_id
   prefix_sg  = var.prefix_sg
   web_server = var.web_server
@@ -55,7 +55,7 @@ module "SG_module" {
 
 module "EC2_module" {
   # source         = "./module_vpc1st/EC2_module"
-  source = "github.com/dibyasachi001/simple_vpc_infra/tree/main/EC2_module"
+  source = "github.com/dibyasachi001/simple_vpc_infra/EC2_module"
   ami_id         = var.ami_id
   instance_type  = var.instance_type
   ec2_key_name       = var.ec2_key_name
